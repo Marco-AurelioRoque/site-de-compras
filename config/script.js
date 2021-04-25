55b54f0d49f8703b6b7946f648ebb73f530e0a32
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-04-25 18:14:11 -0300

    uptade

--- config/script.js
+++ config/script.js
@@ -1,20 +1,24 @@
+function fechaModal1(){
+    document.querySelector(".modal-wrapper").style.display = "none"
+}
+
+function fechaModal2(){
+    document.querySelector(".modal-wrapper2").style.display = "none"
+}
+
+function fechaModal3(){
+    document.querySelector(".modal-wrapper3").style.display = "none"
+}
+
+
 function detalhesProduto1(){
     document.querySelector(".modal-wrapper").style.display = "block"
-
-    //Adicionar botao cancelar
-    document.querySelector(".cancelar").style.display = "none"
 }
 
 function detalhesProduto2(){
     document.querySelector(".modal-wrapper2").style.display = "block"
-
-    //Adicionar botao cancelar
-    document.querySelector(".cancelar").style.display = "none"
 }
 
 function detalhesProduto3(){
     document.querySelector(".modal-wrapper3").style.display = "block"
-
-    //Adicionar botao cancelar
-    document.querySelector(".cancelar").style.display = "none"
 }
