aa006d76dffc994e78f49a31adacbb8fe9c1c852
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-05-07 19:14:19 -0300

    update

--- js/script.js
+++ js/script.js
@@ -21,25 +21,32 @@ function addProduto(){
         document.querySelector(".statusValorInvalido").innerHTML = "ERROR: Valor invalido, tente outro"
         document.querySelector(".statusValorInvalido").style.display = "block"
         document.querySelector(".statusEnvioSucesso").style.display = "none"
+        document.querySelector(".produto-cart-span").innerHTML = ""
+        document.querySelector(".resultado-produtos").innerHTML = ""
+        document.querySelector(".resultado-total-produto").innerHTML = ""
+        document.querySelector(".resultado-total").innerHTML = " R$ " + "0,00"
+        document.querySelector(".statusEnvioSucesso").style.display = "none"
+        document.querySelector(".quantidadeProduto").value = ""
     } else {
         document.querySelector(".statusValorInvalido").style.display = "none"
         document.querySelector(".statusEnvioSucesso").style.display = "block"
+        document.querySelector(".produto-cart-span").innerHTML = "X-BURGUER"
+    
+        //Status
+         document.querySelector(".statusEnvioSucesso").innerHTML = "Produtos adicionados ao carrinho"
+         
+         document.querySelector(".statusEnvioSucesso").style.display = "block"
+     
+         //Resultado no carrinho
+         document.querySelector(".resultado-total").innerHTML = " R$ " + produtoAdicionadoFinal + ",00"
+         
+         //resultado produtos cart
+     
+         document.querySelector(".resultado-produtos").innerHTML = produtoAdicionadoStatus
+         document.querySelector(".resultado-total-produto").innerHTML = produtoAdicionadoFinal + ",00"
     }
 
-    document.querySelector(".produto-cart-span").innerHTML = "X-BURGUER"
-    
-   //Status
-    document.querySelector(".statusEnvioSucesso").innerHTML = "Produtos adicionados ao carrinho"
-    
-    document.querySelector(".statusEnvioSucesso").style.display = "block"
 
-    //Resultado no carrinho
-    document.querySelector(".resultado-total").innerHTML = " R$ " + produtoAdicionadoFinal + ",00"
-    
-    //resultado produtos cart
-
-    document.querySelector(".resultado-produtos").innerHTML = produtoAdicionadoStatus
-    document.querySelector(".resultado-total-produto").innerHTML = produtoAdicionadoFinal + ",00"
 
 }
 
